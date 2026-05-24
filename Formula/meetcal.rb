65feae9e616d4ce9cf106e79d4217e7c0fa94f54
Maddisen Mohnsen <memohnsen@gmail.com>
2026-05-24 17:22:35 -0400

    brew

--- Formula/meetcal.rb
+++ Formula/meetcal.rb
@@ -1,34 +1,33 @@
 class Meetcal < Formula
   desc "CLI for querying MeetCal lifting data"
-  homepage "https://github.com/meetcal/meetcal-cli-rust"
+  homepage "https://github.com/meetcal/meetcal-cli"
   version "1.0.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/meetcal/meetcal-cli-rust/releases/download/v1.0.0/darwin-arm64.tar.gz"
-      sha256 "d61ca36ef5486799414e213a137a4e284ca6641e22a127afa1835925087f9f7f"
+      url "https://github.com/meetcal/meetcal-cli/releases/download/v1.0.0/darwin-arm64.tar.gz"
+      sha256 "74311505eab231e660a362be149226a5352fb1ba4e99125da7246a23fcc5b389"
     else
-      url "https://github.com/meetcal/meetcal-cli-rust/releases/download/v1.0.0/darwin-x64.tar.gz"
-      sha256 "30dacdd51f6a0732fe88b5bf629bcd3aecafc41855cc199312e8f5ec6c70e5f2"
+      url "https://github.com/meetcal/meetcal-cli/releases/download/v1.0.0/darwin-x64.tar.gz"
+      sha256 "64b2b89959177644130f568cd61fc2646f1be4dc964b20f503efb40234dc26f6"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/meetcal/meetcal-cli-rust/releases/download/v1.0.0/linux-arm64.tar.gz"
-      sha256 "efbacb33e7b301fe432c2017e2d473e8872e140c44328e8bb1d1b08e08873b2a"
+      url "https://github.com/meetcal/meetcal-cli/releases/download/v1.0.0/linux-arm64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
     else
-      url "https://github.com/meetcal/meetcal-cli-rust/releases/download/v1.0.0/linux-x64.tar.gz"
-      sha256 "a1a895a1c554778cf61be87c77bbcc75fbee499962bf06ce900bbf5ed7eedaa2"
+      url "https://github.com/meetcal/meetcal-cli/releases/download/v1.0.0/linux-x64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
     end
   end
 
   def install
-    executable = File.exist?("index") ? "index" : Dir["*/index"].first
-    bin.install executable => "meetcal"
+    bin.install "meetcal"
   end
 
   test do
-    system "#{bin}/meetcal", "help"
+    system bin/"meetcal", "--help"
   end
 end
---
+++ README.md
@@ -0,0 +1,29 @@
+# meetcal Homebrew Tap
+
+## Install
+
+```sh
+brew tap meetcal/tap
+brew install meetcal
+```
+
+## Upgrade
+
+```sh
+brew update
+brew upgrade meetcal
+```
+
+## Verify
+
+```sh
+meetcal --help
+```
+
+## Tap repo
+
+https://github.com/meetcal/homebrew-tap
+
+## CLI repo
+
+https://github.com/meetcal/meetcal-cli
